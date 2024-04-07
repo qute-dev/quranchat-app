f91598a3a086527694e99f12c03464903d9afe25
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-04-07 23:06:59 +0700

    feat: add api service

---
+++ src/services/config.ts
@@ -0,0 +1,6 @@
+const isLocal =
+  location.href.includes('localhost') || location.href.includes('127.0.0.1');
+
+export const API_URL = isLocal
+  ? 'http://127.0.0.1:7777'
+  : 'https://nlp.quranchat.me';
