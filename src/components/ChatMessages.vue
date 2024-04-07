26ca379da51de9d94c1b5e1bce0609666b303fb0
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-04-07 23:08:17 +0700

    feat: update ui message

--- src/components/ChatMessages.vue
+++ src/components/ChatMessages.vue
@@ -3,10 +3,13 @@
     <!-- MESSAGES  -->
     <q-scroll-area ref="msgScroll" style="height: 70vh;">
       <q-list class="q-px-md">
-        <template v-for="(msg, index) in chatStore.filteredMsgs" :key="index">
-          <q-chat-message :name="msg.name" :text="[msg.text]" :sent="msg.from === 'me'"
-            :bg-color="msg.from === 'me' ? 'teal-2' : 'grey-1'" :text-color="msg.from === 'me' ? 'black' : 'black'"
-            class="cursor-pointer" @click="msg.from === 'bot' && copyText(msg.text)" />
+        <template v-for="(msg) in chatStore.filteredMsgs" :key="msg.id">
+          <q-chat-message :text="generateTexts(msg)" :text-html="msg.from === 'bot'" :sent="msg.from === 'me'"
+            :bg-color="msg.from === 'me' ? 'teal-2' : 'grey-3'" :text-color="msg.from === 'me' ? 'black' : 'black'"
+            class="cursor-pointer" @click="msg.from === 'bot'" />
+          <div v-if="msg.from === 'bot' && msg.data?.next" class="full-width text-center">
+            <q-btn dense rounded outline color="primary" label="Lanjut" class="q-px-sm" @click="nextResult" />
+          </div>
         </template>
       </q-list>
     </q-scroll-area>
@@ -32,6 +35,7 @@ import { ionPaperPlaneOutline } from '@quasar/extras/ionicons-v7';
 
 import { useChatStore } from 'src/stores/chat-store';
 import { sleep } from 'src/utils';
+import { Message } from 'src/models';
 
 const chatStore = useChatStore();
 const $q = useQuasar();
@@ -54,14 +58,54 @@ async function scrollToBottom() {
   msgScroll?.value?.setScrollPercentage('vertical', 1);
 }
 
+function generateTexts(msg: Message) {
+  const texts = [msg.text || ''];
+
+  const chapter = msg.data?.chapter;
+
+  if (chapter) {
+    let title = `QS ${chapter.name}`;
+
+    if (msg.data?.verses?.length === 1) title += `:${msg.data?.verses[0].verse}`
+
+    texts.push(title);
+  }
+
+  if (msg.data?.verses && msg.data?.translations) {
+    for (let i = 0; i < msg.data.verses.length; i++) {
+      const verse = msg.data.verses[i];
+      const trans = msg.data.translations[i];
+
+      if (!chapter) {
+        texts.push(`QS ${verse.chapter}:${verse.verse}`);
+      }
+
+      texts.push(`<div class="text-right text-h5">${verse.text}<span class="text-caption"> (${verse.verse})</span></div>`);
+      texts.push(`<div class="text-italic"">${trans.text}</div>`);
+    }
+  }
+
+  return texts;
+}
+
 async function sendMessage() {
   if (!msgText.value || chatStore.method === 'llm') return;
 
+  const offset = msgScroll?.value?.getScrollPosition();
+
   await chatStore.sendMessage(msgText.value);
   msgText.value = '';
-  await scrollToBottom();
+
+  msgScroll?.value?.setScrollPosition('vertical', (offset?.top || 0) + 400, 250);
 };
 
+async function nextResult() {
+  msgText.value = 'lanjut';
+
+  await sendMessage();
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function copyText(text: string) {
   await copyToClipboard(text);
   $q.notify({ message: 'Text copied to clipboard', timeout: 500, color: 'info' });
