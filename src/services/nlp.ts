2537ba3e6ce45220ad8d390f2c27c2dabad04dfb
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-04-08 19:38:25 +0700

    fix: platform on query

--- src/services/nlp.ts
+++ src/services/nlp.ts
@@ -1,14 +1,23 @@
 import axios from 'axios';
+import { useQuasar } from 'quasar';
+
 import { API_URL } from './config';
 import { Message } from 'src/models';
+
+const $q = useQuasar();
 
 export async function getReply(text: string, user: string) {
   const url = `${API_URL}/query`;
 
   // console.log('[SERVICE:NLP] getReply', { url, text });
+  const platform = $q.platform.is.android
+    ? 'android'
+    : $q.platform.is.ios
+    ? 'ios'
+    : 'web';
 
   const msg = {
-    platform: 'web',
+    platform,
     user,
     text,
   };
