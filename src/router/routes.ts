048d5eda2cd2308df2ec7928e6e3cc30722e5157
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-04-08 22:56:53 +0700

    feat: add privacy page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
-
+  {
+    path: '/privacy',
+    component: () => import('pages/PrivacyPage.vue'),
+    meta: { title: 'Privacy Policy' },
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
