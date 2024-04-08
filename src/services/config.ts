6788e3edfa674aebe0df771e534dc6c0bfd71bbb
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-04-08 19:34:17 +0700

    fix: config on mobile app

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,8 @@
 const isLocal =
-  location.href.includes('localhost') || location.href.includes('127.0.0.1');
+  window &&
+  window.location &&
+  (location?.href.includes('localhost') ||
+    location?.href.includes('127.0.0.1'));
 
 export const API_URL = isLocal
   ? 'http://127.0.0.1:7777'
