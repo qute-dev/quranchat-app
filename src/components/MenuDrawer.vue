cc0fb9af615c77e9d556f8850a0436c5a68e7403
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-04-11 00:05:48 +0700

    feat: add menu install

--- src/components/MenuDrawer.vue
+++ src/components/MenuDrawer.vue
@@ -3,6 +3,7 @@
     <q-card class="full-width">
       <q-list>
         <menu-item title="BERANDA" :icon="ionHomeOutline" to="/" />
+        <menu-item title="INSTALL APP" :icon="ionLogoGooglePlaystore" to="/apk" v-if="!isMobile()" />
         <menu-item title="INSTAGRAM" :icon="ionLogoInstagram" href="https://instagram.com/quranchat.me"
           target="_blank" />
         <menu-item title="QUTE RESEARCH" :icon="ionEggOutline" href="https://qute.dev" target="_blank" />
@@ -12,14 +13,18 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useQuasar } from 'quasar';
 import {
   ionEggOutline,
   ionHomeOutline,
   ionLogoInstagram,
+  ionLogoGooglePlaystore,
 } from '@quasar/extras/ionicons-v7'
 
 import MenuItem from './MenuItem.vue';
-import { computed } from 'vue';
+
+const $q = useQuasar();
 
 const props = defineProps(['modelValue'])
 const emit = defineEmits(['update:modelValue'])
@@ -32,5 +37,9 @@ const value = computed({
     emit('update:modelValue', value)
   }
 })
+
+const isMobile = () => {
+  return $q.platform.is.nativeMobileWrapper;
+}
 
 </script>
