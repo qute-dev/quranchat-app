3084ba3e84ad0b0e9bed91d51a85a6ee9a010be1
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-04-12 13:54:09 +0700

    fix: local config

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,10 +1,7 @@
-import { Platform } from 'quasar';
-
 const isLocal =
   window?.location?.href?.includes('localhost') ||
   window?.location?.href?.includes('127.0.0.1');
 
-export const API_URL =
-  isLocal && Platform.is.desktop
-    ? 'http://127.0.0.1:7777'
-    : 'https://nlp.quranchat.me';
+export const API_URL = isLocal
+  ? 'http://127.0.0.1:7777'
+  : 'https://nlp.quranchat.me';
