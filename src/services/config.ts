52dd3ebda1325ef6535349d8ddeca77e4e1b3cf0
mdedirudianto <mdedirudianto@gmail.com>
2025-02-19 12:50:53 +0700

    chore: update api url

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,10 @@
-const isDev = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
+const isLocal =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1';
 
-export const API_URL = isDev
+const isDev = location.hostname === '154.26.130.96';
+
+export const API_URL = isLocal
   ? 'http://127.0.0.1:7777'
+  : isDev
+  ? 'http://154.26.130.96:7002'
   : 'https://api.qute.my';
