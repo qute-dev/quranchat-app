4cab097ac83ae981b3ec570bf3dec9d5b4be689c
mdedirudianto <mdedirudianto@gmail.com>
2025-02-27 23:21:00 +0700

    feat: update privacy page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,17 +4,19 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        path: '/privacy',
+        component: () => import('pages/PrivacyPage.vue'),
+        meta: { title: 'Privacy Policy' },
+      },
+    ],
   },
   {
     path: '/apk',
     component: () => import('pages/InstallPage.vue'),
     props: { store: 'google' },
-  },
-  {
-    path: '/privacy',
-    component: () => import('pages/PrivacyPage.vue'),
-    meta: { title: 'Privacy Policy' },
   },
   {
     path: '/:catchAll(.*)*',
